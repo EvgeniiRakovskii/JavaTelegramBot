140b6ef224c9c12b992d942abf09c32f7fdd35ba
EvgeniiRakovskii <ev.rakovskii@yandex.ru>
2021-12-08 23:00:25 +0300

    fix

--- src/main/java/rays/telegram/bot/Bot.java
+++ src/main/java/rays/telegram/bot/Bot.java
@@ -13,30 +13,32 @@ import rays.telegram.constants.Constants;
 import rays.telegram.exceptions.AppException;
 import rays.telegram.services.StockService;
 
+import java.util.logging.Logger;
+
 /**
  * Подправить кратность оругления, у всех акций есть свой параметр minPriceIncrement
  * Добавить последнюю новость по компании
  * Подправить код бота, слишком много проверок, надо их как-то разнести
  * Проверить многопоточность, как работает при многих запросах
  * Эффективность
+ * Загрузить бота на хероку
+ * Тесты?!
+ * Too many requests
  */
 
 public class Bot {
-    // Create  bot passing the token received from @BotFather
+
+    // Creating bot passing the token received from @BotFather
     private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+    private static final Logger logger = Logger.getLogger(Bot.class.getName());
 
     private final StockService stockService = new StockService();
 
-    public Bot() {
-    }
-
     public void serve() {
 
-        // Register for updates
+        // Registering for updates
         bot.setUpdatesListener(updates -> {
             updates.forEach(this::process);
-            // ... process updates
-            // return id of last processed update or confirm them all
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
 
@@ -48,27 +50,33 @@ public class Bot {
         BaseRequest request = null;
 
         if (message != null && message.text() != null) {
-            String messageToBot = message.text().replace("@RaysJavaBot","");
+            String messageToBot = message.text().replace(System.getenv("BOT_NAME"),"");
             long chatId = message.chat().id();
             if (Constants.BOT_COMMANDS.containsKey(messageToBot.toLowerCase())){
                 request = new SendMessage(chatId, Constants.BOT_COMMANDS.get(messageToBot.toLowerCase()));
-            }
-            else
+            } else
             {
-                StockInfo stockInfo;
-                try {
-                    stockInfo = stockService.getInfoAboutStock(messageToBot.replace("/", ""));
-                    StockPrice stockPrice = stockInfo.getStockPrice();
-                    request = new SendMessage(chatId, stockInfo.getStockPrice().getTradeStatus().equals("NotAvailableForTrading") ?
-                            String.format("Торги этим тикетом сейчас не ведутся %nЦена закрытия %s %.2f", stockInfo.getName(), stockPrice.getClosePrice()) :
-                            String.format("Текущая цена %s %.2f %nПроцент изменения за день %.2f", stockInfo.getName(), stockPrice.getLastPrice(), stockPrice.getPercentageChangeForDay()));
-                } catch (JSONException | AppException e) {
-                    request = new SendMessage(chatId, e.getLocalizedMessage());
-                }
+                request = getMessageFromStock(chatId,messageToBot);
             }
         }
         if (request != null) {
+            logger.info("Запросили " + message.text());
             bot.execute(request);
         }
+    }
+
+    private SendMessage getMessageFromStock(long chatId, String messageToBot) {
+        StockInfo stockInfo;
+
+        try {
+            stockInfo = stockService.getInfoAboutStock(messageToBot.replace("/", ""));
+            StockPrice stockPrice = stockInfo.getStockPrice();
+            return new SendMessage(chatId, stockInfo.getStockPrice().getTradeStatus().equals("NotAvailableForTrading") ?
+                    String.format("Торги этим тикетом сейчас не ведутся %nЦена закрытия %s %.2f", stockInfo.getName(), stockPrice.getClosePrice()) :
+                    String.format("Текущая цена %s %.2f %nПроцент изменения за день %.2f", stockInfo.getName(), stockPrice.getLastPrice(), stockPrice.getPercentageChangeForDay()));
+        } catch (JSONException | AppException e) {
+            return new SendMessage(chatId, e.getLocalizedMessage());
+        }
+
     }
 }
