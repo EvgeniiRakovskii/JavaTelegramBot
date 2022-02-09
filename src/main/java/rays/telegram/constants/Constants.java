df3089d93bd0b2d6a1dfe3fb7a515c25724ee5ca
EvgeniiRakovskii <ev.rakovskii@yandex.ru>
2022-02-09 22:21:04 +0300

    fix ticket

--- src/main/java/rays/telegram/constants/Constants.java
+++ src/main/java/rays/telegram/constants/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     static {
         BOT_COMMANDS = new HashMap<>();
         BOT_COMMANDS.put("/help", "Привет! Я биржевой помощник, если пропишешь мне тикет - выдам тебе информацию по текущей цене этой компании\nНапример /TSLA\nПосмотреть другие примеры /examples");
-        BOT_COMMANDS.put("/examples", "Вот мои самые любимые тикеты: \nГазпром - /GAZP\nСбербанк - /SBER\nAlibaba - /BABA\nMail(VK) - /MAIL\nВТБ - /VTBR \nМечел - /MTLR \nРаскадская - /RASP");
+        BOT_COMMANDS.put("/examples", "Вот мои самые любимые тикеты: \nГазпром - /GAZP\nСбербанк - /SBER\nAlibaba - /BABA\nMail(VK) - /VKCO\nВТБ - /VTBR \nМечел - /MTLR \nРаспадская - /RASP");
 
     }
 
